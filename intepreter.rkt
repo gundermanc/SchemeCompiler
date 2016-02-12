cf9352f7d86d99b00e0d60a97f05d640bb85944f
Christian Gunderman <Christian Gunderman>
2016-02-11 20:09:22 -0500

    Single value function.

--- intepreter.rkt
+++ intepreter.rkt
@@ -13,7 +13,7 @@
     (caddr expression)))
 
 ;; Looks up an arithmetic function by its symbol.
-(define numeric_operation_function
+(define operation_function
   (lambda (operator)
     (cond
       ((eq? '+ operator) (lambda (a b) (+ a b)))
@@ -22,12 +22,6 @@
       ((eq? '/ operator) (lambda (a b) (quotient a b)))
       ((eq? '% operator) (lambda (a b) (remainder a b)))
       ((eq? 'return operator) (lambda (a) (M_value a)))
-      )))
-
-;; Looks up a boolean operation function by its symbol.
-(define boolean_operation_function
-  (lambda (operator)
-    (cond
       ((eq? '< operator) (lambda (a b) (< a b)))
       ((eq? '> operator) (lambda (a b) (> a b)))
       ((eq? '<= operator) (lambda (a b) (<= a b)))
@@ -38,22 +32,15 @@
       ((eq? '|| operator) (lambda (a b) (or a b)))
       )))
 
+
 ;; Gets the value of an expression.
 (define M_value
   (lambda (expression)
     (cond
       ((number? expression) expression)
-      (else ((numeric_operation_function (operator expression))
-             (M_value (operand_1 expression))
-             (M_value (operand_2 expression)))))))
-
-; Gets the value of a boolean expression.
-(define M_boolean
-  (lambda (expression)
-    (cond
       ((eq? true expression) #t)
       ((eq? false expression) #f)
-      ((eq? '! (operator expression)) (not (operand_1 expression)))
-      (else ((boolean_operation_function (operator expression))
+      ((eq? '! (operator expression)) (not (M_value (operand_1 expression))))
+      (else ((operation_function (operator expression))
              (M_value (operand_1 expression))
              (M_value (operand_2 expression)))))))
